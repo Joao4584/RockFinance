9adeaaaf714173a3cb30f16d877c994c7df2b523
João Victor Roberto <84518978+Joao4584@users.noreply.github.com>
2023-02-17 02:42:42 -0300

    automatic branch

---
+++ web/src/app/index.tsx
@@ -0,0 +1,13 @@
+export function App() {
+    return (
+        <header>
+            <div className="class">
+                dwa <p>dwa</p>{" "}
+                <div>
+                    {" "}
+                    <p className="a"></p>
+                </div>
+            </div>
+        </header>
+    );
+}
